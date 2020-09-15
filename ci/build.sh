1af0945df9278b32585cb9f96ad526b2807c4f27
Dominik Rosiek <drosiek@sumologic.com>
2020-09-15 10:26:15 +0200

    [sumologic] Add travis and build script

---
+++ ci/build.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+set -e
+
+VERSION="${TRAVIS_TAG:-0.0.0}"
+
+function setup_github() {
+  # Set up Github
+  if [ -n "$GITHUB_TOKEN" ]; then
+    git config --global user.email "travis@travis-ci.org"
+    git config --global user.name "Travis CI"
+    git remote add origin-repo "https://${GITHUB_TOKEN}@github.com/SumoLogic/influxdata-helm-charts.git" > /dev/null 2>&1
+    git fetch --unshallow origin-repo
+    git checkout "${TRAVIS_TAG}"
+  fi
+}
+
+function push_helm_chart() {
+  local project="$1"
+  local version="$2"
+
+  echo "Pushing new Helm Chart release $version"
+  set -x
+  git checkout -- .
+  sudo helm package "charts/${project}" --dependency-update --version="${version}" --app-version="${version}"
+  git fetch origin-repo
+  git checkout gh-pages
+  sudo helm repo index ./ --url "https://sumologic.github.io/influxdata-helm-charts/"
+  git add -A
+  git commit -m "Push new Helm Chart release $version for $project"
+  git push --quiet origin-repo gh-pages
+  set +x
+}
+
+if [[ $VERSION == telegraf-operator/v* ]]; then
+  VERSION="${VERSION#telegraf-operator/v}"
+  setup_github
+  push_helm_chart telegraf-operator "${VERSION}"
+fi
